5718b37fb45dceabc255536253447462bc2b117d
mapan <mapan1984@outlook.com>
2016-11-26 22:51:09 +0800

    init set

---
+++ set/set_use_tree.scm
@@ -0,0 +1,40 @@
+;; 使用二叉树表示集合
+;; 二叉树根结点左边的结点均比它小
+;; 二叉树根结点右边的结点均比它大
+;; (entry left-branch right-branch)
+
+;; set_tree
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left-branch right-branch)
+  (list entry left-branch right-branch))
+
+;; set_tree operate
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((= x (entry set)) #t)
+        ((< x (entry set))
+         (element-of-set? x (left-branch set)))
+        ((> x (entry set))
+         (element-of-set? x (right-branch)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set)
+                    (adjoin-set x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set)
+                    (left-branch set)
+                    (adjoin-set x (right-branch set))))))
+
+;; set_tree --> list
+(define (tree->list tree)
+  (if (null? tree)
+      '()
+      (append (tree->list (left-branch tree))
+              (cons (entry tree)
+                    (tree->list (right-branch tree))))))
