89d54b58640a6ec58142a1304ce66aa07d501d9c
mapan1984 <mapan1984@outlook.com>
2017-07-11 09:55:43 +0800

    max subarray

--- max_subarray/max_subarray.py
+++ max_subarray/max_subarray.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 """ 最大子数组问题的分治法解决
-
+在数组的一系列值中，找到连续和最大的子数组
 """
 
 import random
+
 
 inf = float('Inf')
 ninf = float('-Inf')
@@ -18,11 +19,15 @@ def mid_index(a):
     return (0 + high_index(a)) // 2
 
 def find_max_crossing_subarray(a, low, mid, high):
+    """ 找到包含中点的子数组最大和
+    返回这个子数组的区间[max_left, max_right]
+                    与最大和left_sum+right_sum
+    """
 
     left_sum = ninf
     sum = 0
     max_left = mid
-    for i in reversed(range(low, mid+1)):
+    for i in range(mid, low-1, -1):
         sum = sum + a[i]
         if sum > left_sum:
             left_sum = sum
@@ -40,13 +45,15 @@ def find_max_crossing_subarray(a, low, mid, high):
     return max_left, max_right, left_sum+right_sum
 
 def find_max_subarray(a, low, high):
+    """找到数组的最大子数组"""
     if low == high:
         return low, high, a[low]
     else:
         mid = (low + high) // 2
         left_low, left_high, left_sum = find_max_subarray(a, low, mid)
         right_low, right_high, right_sum = find_max_subarray(a, mid+1, high)
-        cross_low, cross_high, cross_sum = find_max_crossing_subarray(a, low, mid, high)
+        cross_low, cross_high, cross_sum = \
+                find_max_crossing_subarray(a, low, mid, high)
         if left_sum > right_sum and left_sum > cross_sum:
             return left_low, left_high, left_sum
         elif right_sum > left_sum and right_sum > cross_sum:
@@ -61,3 +68,4 @@ def find_max_subarray(a, low, high):
 a = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
 
 print(find_max_subarray(a, 0, high_index(a)))
+
